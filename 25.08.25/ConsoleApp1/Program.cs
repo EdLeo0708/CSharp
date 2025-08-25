7b17cc013dea01d4b4b817a5714f7bc2d7f98dcf
Edson Leonardo Pacheco Navia <edsonleonardo0708@gmail.com>
2025-08-25 10:56:23 -0300

    aula dia25

---
+++ 25.08.25/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static double raio = 0;
+        static void Print(string value) {
+            Console.WriteLine(value);
+        }
+        static void Print(double value)
+        {
+            Console.WriteLine(value);
+        }
+        static double AreadoCirculo(double value)
+        {
+            double r = 0 ;
+            r = Math.Pow(value, 2) * Math.PI;
+            return r;
+        }
+        static void Main(string[] args)
+        {
+            Print("Insira o raio do circulo: ");
+            raio = double.Parse(Console.ReadLine());
+            double resp = AreadoCirculo(raio);
+            Print("Area do circulo "+ resp);
+            Print(resp);
+        }
+    }
+}
