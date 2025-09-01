53c1c3152ec5ee245fd273fe02c34ea7eec0ce98
Edson Leonardo Pacheco Navia <edsonleonardo0708@gmail.com>
2025-09-01 11:35:52 -0300

    ATV3
    Aula 1/9/25 ATVIDADE DE AULA PARA ENTREGAR VIA TEAMS

---
+++ ATV3/ATIVIDADE3/ATIVIDADE3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int altura = 10; // Definindo a altura do triângulo
+
+            while (i < altura)  // Loop externo que controla o número de linhas
+            {
+                int j = 0;
+                int espacos = altura - i - 1;  // Número de espaços para a esquerda
+
+                // Imprime os espaços antes dos asteriscos
+                while (j < espacos)
+                {
+                    Console.Write(" ");
+                    j++;
+                }
+
+                // Imprimindo os asteriscos
+                j = 0;  // Reinicia a variável j
+                while (j <= i)
+                {
+                    Console.Write("* ");
+                    j++;
+                }
+
+                Console.WriteLine();  // Pula para a próxima linha
+                i++;
+            }
+        }
+    }
+}
