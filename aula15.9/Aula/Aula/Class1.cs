f8749dba950272b02e1112067e0f45c608b5a31b
Edson Leonardo Pacheco Navia <edsonleonardo0708@gmail.com>
2025-09-15 11:21:23 -0300

    Aula dia 15
    ...

---
+++ aula15.9/Aula/Aula/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] listaNumeros = { 5, 6, 4, 13 };
+            int numero = 0;
+            while (true)
+            {
+                Console.WriteLine("Tente descobrir um número: ");
+                numero = int.Parse(Console.ReadLine());
+                for (int i = 0; i < listaNumeros.Length; i++)
+                {
+                    if (numero == listaNumeros[i])
+                    {
+                        Console.WriteLine("Parabéns, você acertou!");
+                        if (i >= listaNumeros.Length - 1)
+                        {
+                            Console.WriteLine("Você descobriu todos os números!");
+                            return;
+                        }
+                        numero = int.Parse(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Errou!");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ aula15.9/Aula/Aula/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+static void Main(string[] args)
+{
+    // Array de números
+    int[] listaNumeros = { 5, 6, 4, 13 };
+
+    Console.WriteLine("Digite 4 números separados por espaço:");
+    string[] entrada = Console.ReadLine().Split(' ');
+
+    // Verifica se o usuário digitou 4 números
+    if (entrada.Length != listaNumeros.Length)
+    {
+        Console.WriteLine("Você deve digitar exatamente 4 números.");
+        return;
+    }
+
+    // Converte a entrada para inteiros e verifica se todos os números estão corretos
+    bool senhaCorreta = true;
+    for (int i = 0; i < listaNumeros.Length; i++)
+    {
+        if (!int.TryParse(entrada[i], out int numero) || numero != listaNumeros[i])
+        {
+            senhaCorreta = false;
+            break;
+        }
+    }
+
+    if (senhaCorreta)
+    {
+        Console.WriteLine("Senha correta! Acesso permitido.");
+    }
+    else
+    {
+        Console.WriteLine("Senha incorreta! Acesso negado.");
+    }
+}
