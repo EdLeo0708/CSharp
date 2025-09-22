a661e277d604e552fd5cedbadcfe7c189c825dcb
Edson Leonardo Pacheco Navia <edsonleonardo0708@gmail.com>
2025-09-22 11:30:21 -0300

    Aula pupupi
    :D

---
+++ Aula22.9/Aula 5 Orientação a Objeto ​/Aula 5 Orientação a Objeto ​/Program.cs
@@ -0,0 +1,16 @@
+﻿using Aula_5_Orientação_a_Objeto_;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo veiculo = new Veiculo(777);
+            veiculo.Ligar();
+            veiculo.Placa = "ABC-1234";
+            Console.WriteLine(veiculo.Placa);
+        }
+    }
+}
---
+++ Aula22.9/Aula 5 Orientação a Objeto ​/Aula 5 Orientação a Objeto ​/Veiculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_5_Orientação_a_Objeto_​
+{
+    internal class Veiculo
+    {
+        private string placa;
+        private int codFabrica;
+
+        public Veiculo(int codFabrica)
+        {
+            this.codFabrica = codFabrica;
+        }
+
+        public string Placa { get => placa; set => placa = value; }
+        /*
+        public void Ligar()
+        {
+            Console.WriteLine("Ligado");
+        }
+        */
+        public void Ligar()
+        {
+            Console.WriteLine("Ligado");
+        }
+    }
+}
