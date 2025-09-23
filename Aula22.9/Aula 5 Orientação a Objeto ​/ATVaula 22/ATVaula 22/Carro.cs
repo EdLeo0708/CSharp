c7776254f0be1e6c8541e6d19b740c4b34199f38
Edson Leonardo Pacheco Navia <edsonleonardo0708@gmail.com>
2025-09-23 12:06:27 -0300

    Atividade de aula
    criando lista de carros e classes

---
+++ Aula22.9/Aula 5 Orientação a Objeto ​/ATVaula 22/ATVaula 22/Carro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATVaula_22
+{
+    internal class Carro
+    {   // Propriedades
+        public string Chassis { get; set; }
+        public string LicensePlate { get; set; }
+        public string Cor { get; set; }
+        public string Motor { get; set; }
+
+        // Construtor
+        public Carro(string chassis, string licensePlate, string cor, string motor)
+        {
+            Chassis = chassis;
+            LicensePlate = licensePlate;
+            Cor = cor;
+            Motor = motor;
+        }
+
+        // Método para exibir os detalhes do carro
+        public void MostrarDetalhes()
+        {
+            Console.WriteLine($"Detalhes do carro: Chassis: {Chassis}, Placa: {LicensePlate}, Cor: {Cor}, Motor: {Motor}");
+        }
+    }
+}
---
+++ Aula22.9/Aula 5 Orientação a Objeto ​/ATVaula 22/ATVaula 22/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ATVaula_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lista de 10 carros
+            Carro[] carList = new Carro[10];
+            carList[0] = new Carro("Sedan", "ABC123", "Vermelho", "V6");
+            carList[1] = new Carro("SUV", "XYZ789", "Azul", "Elétrico");
+            carList[2] = new Carro("Hatchback", "LMN456", "Preto", "1.6 Flex");
+            carList[3] = new Carro("Pickup", "QWE321", "Branco", "V8");
+            carList[4] = new Carro("Conversível", "RTY654", "Amarelo", "2.0 Turbo");
+            carList[5] = new Carro("Coupé", "UIO987", "Cinza", "V6 Biturbo");
+            carList[6] = new Carro("Minivan", "JKL741", "Prata", "2.4 Flex");
+            carList[7] = new Carro("Sedan", "FGH852", "Azul Escuro", "Híbrido");
+            carList[8] = new Carro("SUV", "BNM963", "Verde", "Diesel 3.0");
+            carList[9] = new Carro("Hatchback", "PQR159", "Laranja", "Elétrico");
+
+            // Exibindo os Dados de cada carro
+            foreach (Carro carro in carList)
+            {
+                carro.MostrarDetalhes();
+            }
+            // Manter o console aberto até o usuário pressionar uma tecla
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
