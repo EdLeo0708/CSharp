6291bc27166ad76d81fe2060fb6a2d71db26c448
Edson Leonardo Pacheco Navia <edsonleonardo0708@gmail.com>
2025-10-06 11:53:37 -0300

    CP2 part2

---
+++ CP2/cp2 Eercicio 5/cp2 Eercicio 5/Livro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cp2_Eercicio_5
+{
+    class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoPublicacao { get; set; }
+
+        public Livro(string titulo, string autor, int anoPublicacao)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            AnoPublicacao = anoPublicacao;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"{Titulo} – {Autor} ({AnoPublicacao})");
+        }
+    }
+
+}
---
+++ CP2/cp2 Eercicio 5/cp2 Eercicio 5/Program.cs
@@ -0,0 +1,16 @@
+﻿using cp2_Eercicio_5;
+using System;
+
+namespace ExercicioLivro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro meuLivro = new Livro("Dom Quixote", "Miguel de Cervantes", 1605);
+            meuLivro.ExibirInformacoes();
+
+            Console.ReadLine();
+        }
+    }
+}
